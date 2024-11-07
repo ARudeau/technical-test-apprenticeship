92e0ac927c67a452575d3e9e4a03547ef0711c46
Alexandre Rudeau <contact@arudeau.fr>
2024-11-07 14:58:58 +0100

    5*5 tic tac toe, Players still win with three symbols in a row, column or diagonal.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 var boxes = document.querySelectorAll(".game-box");
-
 var start_Btn = document.getElementById("start-btn");
 var game_message = document.getElementById("game-message");
 var restart_Btn = document.getElementById("restart-btn");
 var index = true;
 var count = 0;
 
-var winningPosible = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+//  5x5
+var winningPossible = [
+  // Lines
+  [0, 1, 2], [1, 2, 3], [2, 3, 4],
+  [5, 6, 7], [6, 7, 8], [7, 8, 9],
+  [10, 11, 12], [11, 12, 13], [12, 13, 14],
+  [15, 16, 17], [16, 17, 18], [17, 18, 19],
+  [20, 21, 22], [21, 22, 23], [22, 23, 24],
+  // Cols
+  [0, 5, 10], [5, 10, 15], [10, 15, 20],
+  [1, 6, 11], [6, 11, 16], [11, 16, 21],
+  [2, 7, 12], [7, 12, 17], [12, 17, 22],
+  [3, 8, 13], [8, 13, 18], [13, 18, 23],
+  [4, 9, 14], [9, 14, 19], [14, 19, 24],
+    //  Descending diagonals
+  [0, 6, 12], [1, 7, 13], [2, 8, 14], [3, 9, 15], [4, 10, 16],
+    // Rising diagonals
+  [20, 16, 12], [21, 17, 13], [22, 18, 14], [23, 19, 15], [24, 20, 16]
+];
 
 let animation = () => {
     game_message.style.transition = "all .5s ease-in-out";
@@ -23,13 +31,19 @@ let animation = () => {
 }
 
 let showWinner = (isWinner) => {
-    boxes.forEach(e => e.disabled = true)
-    game_message.innerHTML = `<p><span>👑</span>Winner is '${isWinner}'</p>`;
+    boxes.forEach(e => e.disabled = true);
+    let message;
+    if (isWinner === "x") {
+        message = "🎉 Player X wins ! 🎉";
+    } else {
+        message = "👏 Player Y wins ! 👏";
+    }
+    game_message.innerHTML = `<p>${message}</p>`;
     animation();
 }
 
 let checkWinner = () => {
-    winningPosible.forEach((posibility) => {
+    winningPossible.forEach((posibility) => {
         var valueOne = boxes[posibility[0]].innerText;
         var valueTwo = boxes[posibility[1]].innerText;
         var valueThree = boxes[posibility[2]].innerText;
@@ -42,7 +56,7 @@ let checkWinner = () => {
 }
 
 let draw = (count) => {
-    if (count === 9) {
+    if (count === 25) {
         game_message.innerText = "Game was Draw🫢";
         animation();
     }
